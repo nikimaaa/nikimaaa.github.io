b11c811de533d96ad8a4883d8403b5b21cee6f5a
Nikita Maluga <nikima.vd@gmail.com>
2020-07-08 14:33:29 +0300

    design finished

--- scripts/master.js
+++ scripts/master.js
@@ -1,25 +1,66 @@
 $(document).ready(function () {
   $('.fa-power-off').click(
     function(){
-      if($(this).data('mode')=="on"){
+      if($(this).hasClass('on')){
+        $(this).removeClass('on');
+        $(this).addClass('off');
         $.get('/?switch=off');
-        $(this).css({
-          'color' : '#5CCFE6',
-          'transform' : 'rotate(360deg)',
-          'text-shadow' : 'none'
-        });
-        $(this).data('mode','off');
       }
       else{
         $.get('/?switch=on');
-        $(this).css({
-          'color' : '#ffffff',
-          'transform' : 'rotate(0deg)',
-          'text-shadow' : '0 0 5px #5CCFE6, 0 0 10px #5CCFE6, 0 0 20px #5CCFE6, 0 0 40px #5CCFE6'
-        });
-        $(this).data('mode','on');
+        $(this).removeClass('off');
+        $(this).addClass('on');
+        $.get('/?switch=off');
       }
     }
   );
+
+  $('.fa-sun').click(
+    function(){
+      $('nav input[type=range]').val('125');
+      $('.fa-sun').css({'transform':`rotate(360deg)`});
+      $.get(`/?brightness=125`);
+    }
+  );
+
+  $('nav input[type=range]').on('input', function () {
+    $.get(`/?brightness=${$(this).val()}`);
+    $('.fa-sun').css({'transform':`rotate(${$(this).val()}deg)`});
+  });
+
+  $('input[name=frequncy]').on('input', function () {
+    $.get(`/?frequncy=${$(this).val()}`);
+    $('.bpm').text(`BPM: ${$(this).val()}`);
+  });
+
+  $('.radio_block').click(function() {
+    let name = $(this).data('name');
+    let value = $(this).data('value');
+    $.get(`/?${name}=${value}`);
+    $('.indicator_active').addClass('indicator');
+    $('.indicator_active').removeClass('indicator_active');
+    $(this).children('.indicator').addClass('indicator_active');
+    $(this).children('.indicator').removeClass('indicator');
+  });
+
+  $('.color').on('change', function () {
+    const colorHex = $(this).val();
+    const targetMode = $(this).attr('name');
+    $('.hex').text(`HEX: ${colorHex}`);
+    const red = parseInt(colorHex.slice(1,3),16);
+    const green = parseInt(colorHex.slice(3,5),16);
+    const blue = parseInt(colorHex.slice(5,7),16);
+    $('.rgb').text(`R:${red} G:${green} B:${blue}`);
+    $.get(`/?${targetMode}R=${red}&${targetMode}G=${green}&${targetMode}B=${blue}`);
+  });
+
+  $('.fa-list').click(function () {  
+    $('.hide_menu').removeClass('hide');
+  });
+
+  $('.fa-times').click(function () {  
+    $('.hide_menu').addClass('hide');
+  });
 });
+
 
